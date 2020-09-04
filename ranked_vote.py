110fe5ea0d365562f4ba1957dff125c92df302ff
Trenhob <54007903+Trenhob@users.noreply.github.com>
2020-09-04 13:53:08 -0400

    Prototype Poll Class

--- ranked_vote.py
+++ ranked_vote.py
@@ -1,1 +1,10 @@
 # !python3
+
+import datetime
+
+class Poll:
+    def __init__(self, choices, title="Untitled Poll", user_level=0):
+        self.choices = choices
+        self.title = title
+        self.expires = True if user_level <= 1 else False
+        #self.expireDate = datetime.datetime.now() + datetime.timedelta(days=7.0)
