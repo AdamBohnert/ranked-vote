073276304d41caa3b64dbb8ee2963af6d16a1670
Trenhob <54007903+Trenhob@users.noreply.github.com>
2020-09-07 19:37:52 -0400

    Update Poll class methods

--- ranked_vote.py
+++ ranked_vote.py
@@ -8,3 +8,26 @@ class Poll:
         self.title = title
         self.expires = True if user_level <= 1 else False
         #self.expireDate = datetime.datetime.now() + datetime.timedelta(days=7.0)
+
+    # Find maximum votes
+    def calc_winner(self):
+        high_val = max(self.choices.items(), key=lambda x: x[1])
+        
+        max_keys = {}
+        for k, v in self.choices.items():
+            if v == high_val[1]:
+                max_keys.update({k: v})
+        
+        return max_keys
+        
+
+
+class RankedPoll(Poll):
+    def calc_winner(self):
+        pass
+
+
+# testing
+p = Poll({"Adam":11, "Katie":25, "Dustin":11})
+
+print(p.calc_winner())
