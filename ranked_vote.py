29bb0ccd9dc18794f3657be7e0615e0c7aaf27eb
Trenhob <54007903+Trenhob@users.noreply.github.com>
2020-09-08 14:55:41 -0400

    Add ranked choice logic (WIP)

--- ranked_vote.py
+++ ranked_vote.py
@@ -33,12 +33,36 @@ class Poll:
 
 
 class RankedPoll(Poll):
-    def calc_winner(self):
-        pass
+    def calc_ranked_winner(self):
+        # Total first-place choices
+        choices_copy = self.choices.copy()
+        top_vote = list(self.calc_winner().items())
+        num_votes = sum(self.choices.values())
+        
+        # If the top choice has 51% or more of total votes, it wins
+        while top_vote[0][1] / num_votes <= 0.51:
+            # Otherwise drop the choice with lowest votes
+            for k, _ in self.calc_loser().items():
+                # TODO: Get these users' next best choices - needs stored by user
+                choices_copy.pop(k)
+            # TODO: Add their next choice to the buckets to be compared 
+            break
+        return top_vote
+
+    def calc_loser(self):
+        low_val = min(self.choices.items(), key=lambda x: x[1])
+        
+        min_keys = {}
+        for k, v in self.choices.items():
+            if v == low_val[1]:
+                min_keys.update({k: v})
+        
+        return min_keys
 
 
 # testing
-p = RankedPoll({"Adam":11, "Katie":25, "Dustin":11}, title="Friends")
+p = RankedPoll({"Adam":30, "Katie":12, "Dustin":11}, title="Friends")
 
 print(p.calc_winner())
-print(p)
+print(p.calc_ranked_winner())
+print(p.calc_loser())
